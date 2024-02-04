770ea33cb7ad2a97324e4e980c949157fb161ca3
Evgeniy <j.akimushkin2003@gmail.com>
2024-02-04 03:14:56 +0300

    2

---
+++ src/easy/Solution1108.java
@@ -0,0 +1,15 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Solution1108 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String address = scanner.nextLine();
+        String ans = defangIPaddr(address);
+        System.out.println(ans);
+    }
+    public static String defangIPaddr(String address) {
+        return address.replace("." , "[.]");
+    }
+}
---
+++ src/easy/Solution2769.java
@@ -0,0 +1,17 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Solution2769 {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int t = scanner.nextInt();
+        int x = theMaximumAchievableX(num, t);
+        System.out.println(x);
+    }
+    public static int theMaximumAchievableX(int num, int t) {
+        int x = num + (t*2);
+        return x;
+    }
+}
