59448925565a674ee58e471e326d5a7b42e5541e
Evgeniy Akimushkin <j.akimushkin2003@gmail.com>
2024-02-25 20:58:24 +0300

    added isValid2 method to Solution20

--- src/stack/easy/Solution20.java
+++ src/stack/easy/Solution20.java
@@ -4,8 +4,9 @@ import java.util.Stack;
 
 public class Solution20 {
     public static void main(String[] args){
-        String s = "(";
+        String s = "()([{(()}])";
         System.out.println(isValid(s));
+        System.out.println(isValid2(s));
     }
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
@@ -21,6 +22,27 @@ public class Solution20 {
             }
             else if (stack.isEmpty() || stack.pop() != c){
                 return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isValid2(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
